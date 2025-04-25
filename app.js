f1bb81a25a3f5ea630932f9013588d11e279c0b8
Elliot Haak <ellihaak100@student.kau.se>
2025-04-25 13:35:40 +0200

    börjat nu

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//Filen app.js är den enda ni skall och tillåts skriva kod i.
+
+const express = require('express');
+const jsDOM = require('jsdom');
+const cookieParser = require('cookie-parser');
+const fs = require('fs');
+const globalObject = require('./servermodules/game-modul');
+const http = require('http');
+
+http.createServer((request, response) => {
+    //jadu
+}).listen(3000);
+
+
+
+
